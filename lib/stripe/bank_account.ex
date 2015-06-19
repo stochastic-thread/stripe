87f924ffa4e997e23003604420fc726ae7442288
Arthur Colle <hello@arthurcolle.com>
2015-06-19 18:38:26 -0400

    Add token and bankaccount object

---
+++ lib/stripe/bank_account.ex
@@ -0,0 +1,24 @@
+defmodule Stripe.BankAccount do
+  @type t :: %Stripe.BankAccount{
+                      id: String.t,
+                  object: String.t,
+                  last4: String.t,
+                  country: String.t,
+                  currency: String.t,
+                  status: String.t,
+                  fingerprint: String.t,
+                  routing_number: String.t,
+                  bank_name: String.t,
+  }
+
+  defstruct 			id: "",
+                  object: "bank_account",
+                  last4: "",
+                  country: "",
+                  currency: "",
+                  status: "",
+                  fingerprint: "",
+                  routing_number: "",
+                  bank_name: "",
+	end
+end
